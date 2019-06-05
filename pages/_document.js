8276cb1a0f0ff44a8538a14bcaf4d1e5bdde9e9c
Sarath <sarath.hacks@gmail.com>
2019-06-05 17:10:08 +0530

    Production start bug :bug: closed #1 - Wrong .babelrc entry for styled component plugin

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -45,3 +44,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import next from "next";
 import server from "./server";
 
-const { ENV, PORT = 3000 } = process.env;
-const dev = ENV !== "production";
+const { NODE_ENV, PORT = 3000 } = process.env;
+const dev = NODE_ENV !== "production";
 const app = next({ dev });
 
 const handler = app.getRequestHandler();
